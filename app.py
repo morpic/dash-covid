5d226bede5038a2af569a46a323915166202767b
morpic <53561348+morpic@users.noreply.github.com>
2025-03-18 21:28:50 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from datetime import datetime, timedelta
 import plotly.graph_objects as go
 
 import dash
-import dash_table
-import dash_core_components as dcc
+from dash import dash_table
+from dash import dcc
 from dash import html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
